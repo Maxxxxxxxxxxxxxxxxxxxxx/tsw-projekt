b422cc803ef5a7e51c33fdcbfc6f7f6b8b518f4e
Maxxxxxxxxxxxxxxxxxxxxx <m.schlamberger.674@studms.ug.edu.pl>
2024-01-17 20:09:34 +0100

    refactored and upgraded util

--- api/src/db/model/util.js
+++ api/src/db/model/util.js
@@ -1,5 +1,42 @@
-// @ts-ignore
+// @ts-nocheck
+
+function isObject(variable) {
+  if (variable !== null && typeof variable === "object") {
+    return true;
+  }
+  return false;
+}
+
+function trimPattern(input) {
+  const patternRegex = /(\w+)\.(\w+)/;
+  const trimmed = input.replace(patternRegex, "$2");
+
+  return trimmed;
+}
+
+// export function _mapRecordsToObject(records) {
+//   return records.map((e) => e._fields[0].properties);
+// }
+
 export function _mapRecordsToObject(records) {
-  // @ts-ignore
-  return records.map((e) => e._fields[0].properties);
+  return records.map((record) => {
+    const newRecord = {};
+
+    record.keys.forEach((key, index) => {
+      newRecord[trimPattern(key)] = isObject(record._fields[index])
+        ? record._fields[index].properties
+        : record._fields[index];
+    });
+
+    return Object.keys(newRecord).reduce((acc, key) => {
+      const property = newRecord[key];
+      if (isObject(property)) {
+        return { ...acc, ...property };
+      } else {
+        let newAcc = acc;
+        newAcc[key] = property;
+        return newAcc;
+      }
+    }, {});
+  });
 }
