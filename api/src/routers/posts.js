79f472835750bb683cc8027a9bc2d61d0e24fc27
Maxxxxxxxxxxxxxxxxxxxxx <m.schlamberger.674@studms.ug.edu.pl>
2024-01-17 21:59:12 +0100

    added post crud routes

--- api/src/routers/posts.js
+++ api/src/routers/posts.js
@@ -1,28 +1,50 @@
 //@ts-nocheck
-
 import express from "express";
-import dotenv from "dotenv";
 import * as postModel from "../db/model/post";
 import { checkAuthenticated } from "../middlewares/isAuth";
+import log from "../log";
 
 const router = express.Router();
 
-// Protected route
-router.get("/protected", checkAuthenticated, (req, res) => {
-  res.json(req.user);
-});
-
+// Create new post
 router.post("/", checkAuthenticated, async function (req, res) {
-  const userId = req.user.userid;
+  const userId = req.user;
   const postContent = req.body.content;
 
+  const createdPost = await postModel.save(userId, postContent);
+
+  res.status(201).json(createdPost);
+});
+
+// Reply to post endpoint
+router.post("/:id", checkAuthenticated, async function (req, res) {
+  const userId = req.user;
+  const postContent = req.body.content;
+  const opId = req.params.id;
+
+  const citationId = req.query["cite"];
+
+  const result = citationId
+    ? await postModel.saveReplyWithCitation(
+        userId,
+        postContent,
+        opId,
+        citationId
+      )
+    : await postModel.saveReply(userId, postContent, opId);
+
+  res.status(201).json(result);
+});
+
+// Get thread view
+router.get("/:id", async function (req, res) {
   const allPosts = await postModel.get();
   res.json(allPosts);
 });
 
 router.get("/", async function (req, res) {
-  const allPosts = await postModel.get();
-  res.json(allPosts);
+  const threads = await postModel.getThreads();
+  res.json(threads);
 });
 
 export default router;
