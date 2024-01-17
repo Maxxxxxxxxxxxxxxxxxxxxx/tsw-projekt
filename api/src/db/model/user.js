eff3c019fc26dfe9904cddf1f1088bc48cef5223
Maxxxxxxxxxxxxxxxxxxxxx <m.schlamberger.674@studms.ug.edu.pl>
2024-01-17 17:58:33 +0100

    user schema updated

--- api/src/db/model/user.js
+++ api/src/db/model/user.js
@@ -1,6 +1,16 @@
 import hash from "../../auth/hash";
 import getSession from "../driver";
 import { v4 as uuid } from "uuid";
+import { z } from "zod";
+import { _mapRecordsToObject } from "./util";
+
+const schema = z.object({
+  username: z.string(),
+  passwordHash: z.string(),
+  userid: z.string().uuid(),
+  bio: z.string().nullable(),
+  image: z.string().nullable(),
+});
 
 export async function getAll() {
   const session = getSession();
@@ -11,9 +21,15 @@ export async function getAll() {
     `
   );
 
-  return res.records.map((e) => e._fields[0].properties);
+  session.close();
+
+  // @ts-ignore
+  return _mapRecordsToObject(res.records);
 }
 
+/**
+ * @param {string} id
+ */
 export async function get(id) {
   const session = getSession();
   const res = await session.run(
@@ -21,12 +37,18 @@ export async function get(id) {
     MATCH (user:User { userid: $id})
     RETURN user;
     `,
-    { userid: id }
+    { id }
   );
 
-  return res.records.map((e) => e._fields[0].properties);
+  session.close();
+
+  // @ts-ignore
+  return _mapRecordsToObject(res.records);
 }
 
+/**
+ * @param {string} username
+ */
 export async function getByUsername(username) {
   const session = getSession();
   const res = await session.run(
@@ -37,9 +59,16 @@ export async function getByUsername(username) {
     { username }
   );
 
-  return res.records.map((e) => e._fields[0].properties);
+  session.close();
+
+  // @ts-ignore
+  return _mapRecordsToObject(res.records);
 }
 
+/**
+ * @param {string} username
+ * @param {string} password
+ */
 export async function save(username, password) {
   const session = getSession();
   const res = await session.run(
@@ -55,18 +84,27 @@ export async function save(username, password) {
   } else {
     const createdUser = await session.run(
       `
-        CREATE (user:User {username: $username, passwordHash: $passwordHash, userid: $userid})
+        CREATE (user:User {
+          username: $username, 
+          passwordHash: $passwordHash, 
+          userid: $userid,
+          bio: \"\",
+          image: \"\",
+        })
         RETURN user;
         `,
       { username, passwordHash: hash(password), userid: uuid() }
     );
 
-    return createdUser.records[0];
+    return _mapRecordsToObject(createdUser.records);
   }
 
   session.close();
 }
 
+/**
+ * @param {string} id
+ */
 export async function deleteById(id) {
   const session = getSession();
   const res = await session.run(
@@ -78,7 +116,7 @@ export async function deleteById(id) {
   );
 
   if (res.records.length != 0) {
-    console.log(`User ${username} not found!`);
+    console.log(`User ${id} not found!`);
   } else {
     await session.run(
       `
@@ -92,4 +130,54 @@ export async function deleteById(id) {
   }
 
   session.close();
+}
+
+/**
+ * @param {string} id
+ * @param {string} newBio
+ */
+export async function updateBio(id, newBio) {
+  const session = getSession();
+  const res = await session.run(
+    `
+      MATCH (user:User {userid: $id})
+      RETURN user;
+      `,
+    { id }
+  );
+
+  if (res.records.length != 0) {
+    const updatedRes = await session.run(
+      `
+      MATCH (user:User {userid: $id})
+      SET user.bio = $newBio
+      RETURN user
+      `,
+      { id, newBio }
+    );
+
+    return _mapRecordsToObject(updatedRes.records);
+  }
+
+  session.close();
+}
+
+/**
+ * @param {string} followerId
+ * @param {string} followedId
+ */
+export async function followUser(followerId, followedId) {
+  if ((await get(followerId)) && (await get(followedId))) {
+    const session = getSession();
+
+    await session.run(
+      `
+      MATCH (followed:User { userid: $followedId })
+      MATCH (follower:User { userid: $followerId })
+      CREATE (follower)-[:FOLLOWS]->(followed)
+      `
+    );
+
+    return { message: `User ${followedId} followed` };
+  }
 }
