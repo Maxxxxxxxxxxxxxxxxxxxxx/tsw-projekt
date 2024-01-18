5bc5abecc9e08b2fa0e978ba476ff0436a0fddcf
Maxxxxxxxxxxxxxxxxxxxxx <m.schlamberger.674@studms.ug.edu.pl>
2024-01-18 14:31:26 +0100

    added authorization middleware

--- api/src/middlewares/isAuth.js
+++ api/src/middlewares/isAuth.js
@@ -1,7 +1,13 @@
-// @ts-ignore
+// @ts-nocheck
 export async function checkAuthenticated(req, res, next) {
   //   console.log(req.user);
   if (req.method == "OPTIONS") return next();
   if (!req?.user) return res.status(403).send();
   return next();
+}
+
+// For user profile related endpoints
+export async function authorizeProfileMethod(req, res, next) {
+  if (!req.params.id || !req?.user) return res.status(403).send();
+  else if (req.user === req.params.id) return next();
 }
