979e021a9586743605200cc0de7f448de75b46ad
Maxxxxxxxxxxxxxxxxxxxxx <m.schlamberger.674@studms.ug.edu.pl>
2024-01-23 19:25:51 +0100

    add authorization

--- api/src/middlewares/isAuth.js
+++ api/src/middlewares/isAuth.js
@@ -8,6 +8,9 @@ export async function checkAuthenticated(req, res, next) {
 
 // For user profile related endpoints
 export async function authorizeProfileMethod(req, res, next) {
-  if (!req.params.id || !req?.user) return res.status(403).send();
-  else if (req.user === req.params.id) return next();
+  console.warn(`Authorize check: ${req.params.id} - ${req.user}`);
+
+  if (!req.params.id || !req?.user) {
+    return res.status(403).send();
+  } else if (req.user === req.params.id) return next();
 }
