1a8106dddb3ea66791e5873a4815f29bf721cc90
Maxxxxxxxxxxxxxxxxxxxxx <m.schlamberger.674@studms.ug.edu.pl>
2024-01-23 19:26:58 +0100

    update cdn controller

--- api/src/middlewares/cdn.js
+++ api/src/middlewares/cdn.js
@@ -1,13 +1,24 @@
 import multer from "multer";
 import path from "path";
+import fs from "fs";
+import log from "../log";
+
+const staticFilesPath = path.join(__dirname, "../static");
 
 const storage = multer.diskStorage({
   destination: function (req, file, next) {
-    next(null, path.join(__dirname, "../static/"));
+    const userDirPath = path.join(staticFilesPath, `/images/${req.user}`);
+
+    if (!fs.existsSync(userDirPath)) {
+      fs.mkdirSync(userDirPath);
+      log.debug(`Created directory ${userDirPath}`);
+    }
+
+    next(null, path.join(userDirPath));
   },
   filename: (req, file, cb) => {
-    // cb(null, Date.now() + path.extname(file.originalname));
-    cb(null, file.originalname);
+    // @ts-ignore
+    cb(null, `${Date.now()}-${req.user}${path.extname(file.originalname)}`);
   },
 });
 
