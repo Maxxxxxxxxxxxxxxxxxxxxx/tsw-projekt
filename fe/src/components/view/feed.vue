15ac7f9df0f885b94166e0d9ef1c2f59c848f4bd
Maxxxxxxxxxxxxxxxxxxxxx <m.schlamberger.674@studms.ug.edu.pl>
2024-01-24 05:11:11 +0100

    overhaul feed / add date picker

--- fe/src/components/view/feed.vue
+++ fe/src/components/view/feed.vue
@@ -1,5 +1,5 @@
 <script>
-import axios from "axios";
+import axios, { toFormData } from "axios";
 import PostCard from "../ui/postCard.vue";
 import PostForm from "../ui/postForm.vue";
 import isLoggedIn from "@/isLoggedIn";
@@ -12,17 +12,42 @@ export default {
     LoginTo,
   },
   data() {
+    const date = new Date().toLocaleDateString();
     return {
-      postsData: [],
+      posts: [],
+      postsPreview: [],
       loaded: false,
       user: {},
       loggedIn: false,
+      start: date,
+      renderPosts: true,
     };
   },
+  watch: {
+    start: function (n, o) {
+      this.rerenderPosts();
+    },
+  },
   async mounted() {
+    const startDated = new Date(this.start);
+    const startTimestamp = startDated.getTime();
+
     try {
-      const res = await axios.get("https://localhost:3000/api/posts");
-      this.postsData = res.data;
+      const res = await axios.get("https://localhost:3000/api/posts/", {
+        withCredentials: true,
+      });
+
+      res.data.sort((a, b) => b.dateposted - a.dateposted);
+
+      this.posts = res.data;
+
+      if (startTimestamp) {
+        this.postsPreview = res.data.filter(
+          (e) => e.dateposted > startTimestamp
+        );
+      } else {
+        this.postsPreview = res.data;
+      }
 
       const auth = await isLoggedIn();
 
@@ -39,6 +64,24 @@ export default {
       console.error("Error fetching data:", error);
     }
   },
+  methods: {
+    randomKey() {
+      return Math.floor(Math.random() * 278000000000000);
+    },
+    async rerenderPosts() {
+      const startDated = new Date(this.start);
+      const startTimestamp = startDated.getTime();
+
+      this.postsPreview = this.posts.filter((e) => {
+        const timestamp = new Date(e.dateposted).getTime();
+        return timestamp > startTimestamp;
+      });
+
+      this.renderPosts = false;
+      await this.$nextTick();
+      this.renderPosts = true;
+    },
+  },
 };
 </script>
 
@@ -47,7 +90,16 @@ export default {
     <div class="container-l feed" v-if="loaded">
       <PostForm v-if="loggedIn" />
       <LoginTo :prompt="'Log in to post'" v-else />
-      <PostCard v-for="item in postsData" :key="item.postid" :data="item" />
+      <div class="date-pick" v-if="loggedIn">
+        <input class="date-pick-input" type="date" v-model="start" />
+        <!-- <input class="date-pick-input" type="date" v-model="time.end" /> -->
+      </div>
+      <PostCard
+        v-for="item in postsPreview"
+        :key="randomKey"
+        :data="item"
+        v-if="renderPosts"
+      />
     </div>
     <div class="container-l feed" v-else>Loading...</div>
   </div>
@@ -62,5 +114,16 @@ export default {
   align-items: center;
   margin: auto;
   width: 50%;
+}
+
+.date-pick {
+  display: flex;
+  flex-direction: row;
+  gap: 0.8rem;
+  margin-bottom: 1rem;
+}
+
+.date-pick-input {
+  border: none;
 }
 </style>
